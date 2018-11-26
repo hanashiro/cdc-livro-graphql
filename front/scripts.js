ebc8bee8411d61cc7666522aa4f5b103918a6215
hanashiro <felipehanashiro@hotmail.com>
2018-11-26 10:01:21 -0200

    front - ws

--- front/scripts.js
+++ front/scripts.js
@@ -2,7 +2,8 @@ const listaAlunos = document.querySelector('#listaAlunos');
 
 
 const GraphQl = {
-	endpoint: 'http://localhost:4000/graphql',
+	endpoint: 'http://localhost:3000',
+	wsConnection: new WebSocket('ws:http://localhost:3000/graphql', 'graphql-subscriptions'),
 	exec: function(query, variaveis){
 		return fetch(GraphQl.endpoint, {
 		  method: 'POST',
@@ -12,6 +13,31 @@ const GraphQl = {
 		  body: JSON.stringify({ query: query, variables: variaveis }),
 		})
   		.then(resposta => resposta.json())
+	},
+	iniciarWS: function(){
+		return new Promise(resolve => {
+				GraphQl.wsConnection.onopen = function(){
+					const mensagem = {
+								type: 'init'
+					};
+					GraphQl.wsConnection.send(JSON.stringify(mensagem));
+
+					GraphQl.wsConnection.onmessage = function (event) {
+						const resposta = JSON.parse(event.data);
+						if(resposta.type === 'subscription_data'){
+							const aluno = resposta.payload.data.aluno;
+							if(aluno.mutation === 'CREATED'){
+								Template.inserirAlunoLista(aluno.node);
+							}else if(aluno.mutation === 'DELETED'){
+								const id = aluno.previousValues.id.replace(/StringIdGCValue\((.*)\)/, '$1');  
+								Template.removerAlunoLista(id);
+							}
+						}
+					}
+
+					resolve();
+			}
+		})
 	}
 }
 
@@ -57,6 +83,30 @@ const Aluno = {
 		}
 		`;
 		return GraphQl.exec(query, {id});
+	},
+	subscription: function(){
+		const query = `
+		subscription updatedAlunos {
+		  aluno(where: {
+		    mutation_in: [CREATED, DELETED]
+		  }){
+		    mutation
+		    node{
+		      id
+		      nomeCompleto
+		      idade
+		    }
+	    	previousValues{
+			  	id
+	    	}
+		  }
+		}
+		`;
+		GraphQl.wsConnection.send(JSON.stringify({
+			id: '1',
+			type: 'subscription_start',
+			query
+		}))
 	}
 }
 
@@ -67,6 +117,8 @@ const Template = {
 				Aluno.lista = alunoes;
 				Template.listarAluno();
 			})
+
+		GraphQl.iniciarWS().then(Aluno.subscription);
 	},
 	listarAluno: function(){
 		let html = ''
@@ -85,10 +137,7 @@ const Template = {
 		if(novoAluno.nomeCompleto && novoAluno.idade){
 			formulario.nomeCompleto.value = '';
 			formulario.idade.value = '';
-			Aluno.criar(novoAluno)
-				.then(({data: {createAluno}}) => {
-					Template.inserirAlunoLista(createAluno);
-				})
+			Aluno.criar(novoAluno);
 		}
 	},
 	inserirAlunoLista: function(novoAluno){
@@ -96,10 +145,7 @@ const Template = {
 		Template.listarAluno();
 	},
 	apagarAluno: function(id){
-		Aluno.apagar(id)
-			.then(() => {
-				Template.removerAlunoLista(id);
-			})
+		Aluno.apagar(id);
 	},
 	removerAlunoLista: function(id){
 		const alunoIndice = Aluno.lista.findIndex(aluno => aluno.id === id);
